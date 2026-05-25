06ff502c87313c0bf656a2c58a3ecae10061e4b4
kkMarcoooo <marcosouzavitto11@gmail.com>
2026-05-25 17:15:04 -0300

    Implement user database and update UI flow
    Add SQLite database for user management and enhance login/register functionality.

--- tigrinho mania.py
+++ tigrinho mania.py
@@ -3,6 +3,16 @@ import sqlite3
 janela = tk.Tk()
 janela.title("GRANDE_ONÇÃO")
 janela.minsize(width=900, height=700)
+conexao = sqlite3.connect("dados_onca")
+funcio = conexao.cursor()
+funcio.execute("""
+    CREATE TABLE IF NOT EXISTS bancoDeDados(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+)
+""")
 def menu():
     for elementos in janela.winfo_children():
         elementos.destroy()
@@ -23,6 +33,17 @@ def login():
     for elementos in janela.winfo_children():
         elementos.destroy()
     janela.config(bg="#403f3e")
+    def pegar_dados():
+        e = email_entry.get().strip()
+        p = password_entry.get().strip()
+        funcio.execute("SELECT email, password FROM bancoDeDados WHERE email = ? AND password = ?",(e,p))
+        busca = funcio.fetchall()
+        if not busca:
+            msg = "INCORRET EMAIL OR PASSWORD"
+        else:
+            lets_go_gambling()
+
+
     # =====~Frame~===========----------
     frame_menu = tk.Frame(janela, bg="#222626").place(relx=0.73,rely=0.5,relwidth=0.5,relheight=0.95,anchor="center")
     # =====~Email~===========----------
@@ -35,10 +56,70 @@ def login():
     passwordLogin.place(relx=0.56, rely=0.45, anchor="center")
     password_entry= tk.Entry(frame_menu, highlightbackground="black", highlightthickness=2, font=("Arial", 14), show="*")
     password_entry.place(relx=0.73,rely=0.5,relwidth=0.4,relheight=0.08,anchor="center")
+    # =====~Send~===========----------
+    send_button = tk.Button(frame_menu, text="Send")
+    send_button.place(relx=0.73,rely=0.65,relwidth=0.25,relheight=0.08, anchor="center")
+
+    tk.Label(frame_menu, text="Create Account", bg="#222626", fg="White", font=("Agency FB", 28,"bold")).place(relx=0.73, rely=0.08, anchor="center")
+    tk.Label(frame_menu, text="🐯", bg="#403f3e", fg="#ffd900", font=("Arial", 90)).place(relx=0.23, rely=0.5,anchor="center")
+    tk.Label(frame_menu, text="O N Ç A", bg="#403f3e", fg="#ffd900", font=("Arial", 50)).place(relx=0.23, rely=0.37,anchor="center")
+    tk.Button(janela,text="Back", command=menu).place(relx=0.04,rely=0.9)
+
+
 def cadastro():
     for elementos in janela.winfo_children():
         elementos.destroy()
     janela.config(bg="#403f3e")
+
+    def pegar_dados():
+        u = username_entry.get()
+        e = email_entry.get()
+        p = password_entry.get()
+        pc = passwordConfirmLogin.get()
+        if u == "" or e == "" or p == "" or pc == "":
+            msg = "FILL IN ALL FIELDS"
+        elif not e.endswith(("@gmail.com", "@outlook.com", "@yahoo.com", "@hotmail.com")):
+            msg = "ENTER A VALID EMAIL"
+        elif p != pc:
+            msg = "PASSWORDS DO NOT MATCH"
+        elif len(p) < 6:
+            msg = "ENTER A VALID PASSWORD"
+        if msg == "":
+            funcio.execute("""INSERT INTO bancoDeDados(username, email, password) VALUES(?,?,?)""", (u,e,p))
+            conexao.commit()
+            menu()
+    # =====~Frame~===========----------
+    frame_menu = tk.Frame(janela, bg="#222626").place(relx=0.73,rely=0.5,relwidth=0.5,relheight=0.95,anchor="center")
+    # =====~Username~===========----------
+    usernameLogin = tk.Label(frame_menu, text="Username", bg ="#222626", fg="white" )
+    usernameLogin.place(relx=0.56, rely=0.19, anchor="center")
+    username_entry = tk.Entry(frame_menu, highlightbackground="black", highlightthickness=2, font=("Arial", 14))
+    username_entry.place(relx=0.73,rely=0.24,relwidth=0.4,relheight=0.08,anchor="center")
+    # =====~Email~===========----------
+    emailLogin = tk.Label(frame_menu, text="Email", bg ="#222626", fg="white" )
+    emailLogin.place(relx=0.55, rely=0.32, anchor="center")
+    email_entry = tk.Entry(frame_menu, highlightbackground="black", highlightthickness=2, font=("Arial", 14))
+    email_entry.place(relx=0.73,rely=0.37,relwidth=0.4,relheight=0.08,anchor="center")
+    # =====~Password~===========----------
+    passwordLogin = tk.Label(frame_menu, text="Password", bg ="#222626", fg="white" )
+    passwordLogin.place(relx=0.56, rely=0.45, anchor="center")
+    password_entry= tk.Entry(frame_menu, highlightbackground="black", highlightthickness=2, font=("Arial", 14), show="*")
+    password_entry.place(relx=0.73,rely=0.5,relwidth=0.4,relheight=0.08,anchor="center")
+    # =====~Password_Confirm~===========----------
+    passwordConfirmLogin = tk.Label(frame_menu, text="Password Confirm", bg ="#222626", fg="white" )
+    passwordConfirmLogin.place(relx=0.59, rely=0.58, anchor="center")
+    passwordConfirm_entry= tk.Entry(frame_menu, highlightbackground="black", highlightthickness=2, font=("Arial", 14), show="*")
+    passwordConfirm_entry.place(relx=0.73,rely=0.63,relwidth=0.4,relheight=0.08,anchor="center")
+    # =====~Send~===========----------
+    send_button = tk.Button(frame_menu, text="Send")
+    send_button.place(relx=0.73,rely=0.8,relwidth=0.25,relheight=0.08, anchor="center")
+    tk.Label(frame_menu, text="Login", bg="#222626", fg="White", font=("Agency FB", 28,"bold")).place(relx=0.73, rely=0.08, anchor="center")
+    tk.Label(frame_menu, text="🐯", bg="#403f3e", fg="#ffd900", font=("Arial", 90)).place(relx=0.23, rely=0.5,anchor="center")
+    tk.Label(frame_menu, text="O N Ç A", bg="#403f3e", fg="#ffd900", font=("Arial", 50)).place(relx=0.23, rely=0.37,anchor="center")
+
+    tk.Button(janela,text="Back", command=menu).place(relx=0.04,rely=0.9)
+
+
 def lets_go_gambling():
     for elementos in janela.winfo_children():
         elementos.destroy()
